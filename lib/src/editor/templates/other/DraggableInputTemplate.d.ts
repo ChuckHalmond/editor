0853c53b78cd780c27f6ff439895ea8453865a52
ChuckHalmond <charles.mecheriki@gmail.com>
2021-09-01 15:49:06 +0200

    update

--- lib/src/editor/templates/other/DraggableInputTemplate.d.ts
+++
@@ -1,8 +0,0 @@
-import { HTMLEDraggableElement } from "editor/elements/controls/draggable/Draggable";
-export { HTMLDraggableInputTemplateDescription };
-export { HTMLDraggableInputTemplate };
-declare type HTMLDraggableInputTemplateDescription = Partial<Pick<HTMLEDraggableElement, 'id' | 'className'>> & Partial<Pick<HTMLInputElement, 'name'>>;
-interface HTMLDraggableInputTemplate {
-    (desc: HTMLDraggableInputTemplateDescription): HTMLEDraggableElement;
-}
-declare const HTMLDraggableInputTemplate: HTMLDraggableInputTemplate;
