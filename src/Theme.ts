fc45a1a7da8beb4c5238ff730993d3043a81325e
ChuckHalmond <charles.mecheriki@gmail.com>
2022-10-27 15:21:50 +0200

    --

--- src/Theme.ts
+++ src/Theme.ts
@@ -1,7 +1,7 @@
 export { themeStylesheet };
 
 const themeStylesheet = new CSSStyleSheet();
-fetch("/css/theme.css").then(
+fetch("../css/theme.css").then(
     async function(resp) {
         const text = await resp.text();
         themeStylesheet.replace(text);
